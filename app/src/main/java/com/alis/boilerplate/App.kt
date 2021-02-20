35ea3a40046e9b0b3a719331c3d9a0efe3171f2b
macbookpro <abenov2005.uzer@gmail.com>
2021-02-21 04:00:59 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/App.kt
+++ app/src/main/java/com/alis/boilerplate/App.kt
@@ -3,8 +3,8 @@ package com.alis.boilerplate
 import android.app.Application
 import com.alis.boilerplate.servicelocator.localModule
 import com.alis.boilerplate.servicelocator.networkModule
-import com.alis.boilerplate.servicelocator.repositoryModule
-import com.alis.boilerplate.servicelocator.viewModelModule
+import com.alis.boilerplate.servicelocator.repositoriesModule
+import com.alis.boilerplate.servicelocator.viewModelsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -14,7 +14,7 @@ class App : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(viewModelModule, repositoryModule, localModule, networkModule)
+            modules(viewModelsModule, repositoriesModule, localModule, networkModule)
         }
     }
 }
--- app/src/main/java/com/alis/boilerplate/servicelocator/Modules.kt
+++ app/src/main/java/com/alis/boilerplate/servicelocator/Modules.kt
@@ -8,11 +8,11 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModelModule = module {
+val viewModelsModule = module {
     viewModel { HomeViewModel(get()) }
 }
 
-val repositoryModule = module {
+val repositoriesModule = module {
     factory { Repository(get()) }
 }
 
