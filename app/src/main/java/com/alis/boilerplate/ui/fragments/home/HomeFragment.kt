f0b0658fb9a382df9ea77a6260a72251e2d98c32
macbookpro <abenov2005.uzer@gmail.com>
2021-02-21 04:15:22 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/alis/boilerplate/ui/fragments/home/HomeFragment.kt
@@ -2,9 +2,9 @@ package com.alis.boilerplate.ui.fragments.home
 
 import androidx.fragment.app.Fragment
 import com.alis.boilerplate.R
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private val viewModel: HomeViewModel by inject()
+    private val viewModel: HomeViewModel by viewModel()
 }
