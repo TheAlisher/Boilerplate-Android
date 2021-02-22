67101c93b97ab349991b94adb054a89036fd7305
macbookpro <abenov2005.uzer@gmail.com>
2021-02-22 10:14:33 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/extensions/PermissionsExtensions.kt
+++ app/src/main/java/com/alis/boilerplate/extensions/PermissionsExtensions.kt
@@ -4,6 +4,10 @@ import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
+object PermissionsConstants {
+    const val PERMISSION_DENIED_WITH_DO_NOT_ASK_AGAIN = 69
+}
+
 fun Fragment.hasPermission(permission: String, requestCode: Int): Boolean {
     return if (ContextCompat.checkSelfPermission(
             requireContext(), permission
@@ -11,21 +15,17 @@ fun Fragment.hasPermission(permission: String, requestCode: Int): Boolean {
     ) {
         true
     } else {
-        requestPermissions(
-            arrayOf(permission),
-            requestCode
-        )
+        requestPermissions(arrayOf(permission), requestCode)
         false
     }
 }
-
 
 fun Fragment.isGranted(grantResults: IntArray, permission: String): Int {
     return if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
         PackageManager.PERMISSION_GRANTED
     } else {
         if (!shouldShowRequestPermissionRationale(permission)) {
-            69
+            PermissionsConstants.PERMISSION_DENIED_WITH_DO_NOT_ASK_AGAIN
         } else {
             PackageManager.PERMISSION_DENIED
         }
