fbcaff3972c10becae029fa376bc715bc08543c9
macbookpro <abenov2005.uzer@gmail.com>
2021-03-04 16:27:04 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/data/PreferencesHelper.kt
+++ app/src/main/java/com/alis/boilerplate/data/PreferencesHelper.kt
@@ -7,8 +7,4 @@ class PreferencesHelper(context: Context) {
 
     private val preferences: SharedPreferences =
         context.getSharedPreferences("boilerplate.preferences", Context.MODE_PRIVATE)
-
-    private fun remove(key: String) {
-        preferences.edit().remove(key).apply()
-    }
 }
