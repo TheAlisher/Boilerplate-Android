b9c8efb42475d8003a60eabfce9c31e96a6f6521
macbookpro <abenov2005.uzer@gmail.com>
2021-03-11 01:22:54 +0600

    added -> Room

--- app/src/main/java/com/alis/boilerplate/data/repositories/KoinRepository.kt
+++ app/src/main/java/com/alis/boilerplate/data/repositories/KoinRepository.kt
@@ -1,7 +1,11 @@
 package com.alis.boilerplate.data.repositories
 
+import com.alis.boilerplate.data.local.RoomDao
 import com.alis.boilerplate.data.network.retrofit.API
 
-class KoinRepository(private val api: API) {
+class KoinRepository(
+    private val api: API,
+    private val roomDao: RoomDao
+) {
     // …
 }
