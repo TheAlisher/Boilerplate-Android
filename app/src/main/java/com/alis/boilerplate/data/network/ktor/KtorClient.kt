f1814874951f2ef41318a68f1acc96bb2c4b33e8
macbookpro <abenov2005.uzer@gmail.com>
2021-03-14 04:34:23 +0600

    removed -> Ktor's OkHttp

--- app/src/main/java/com/alis/boilerplate/data/network/ktor/KtorClient.kt
+++ app/src/main/java/com/alis/boilerplate/data/network/ktor/KtorClient.kt
@@ -2,22 +2,42 @@ package com.alis.boilerplate.data.network.ktor
 
 import com.alis.boilerplate.constants.NetworkConstants
 import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
 import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 class KtorClient {
 
-    private val client = HttpClient {
+    private val client = HttpClient(OkHttp) {
         defaultRequest {
             host = NetworkConstants.HOST
             url {
                 protocol = URLProtocol.HTTPS
             }
         }
+
         install(JsonFeature) {
             serializer = GsonSerializer()
+        }
+
+        engine {
+            addInterceptor(provideLoggingInterceptor())
+
+            config {
+                connectTimeout(30, TimeUnit.SECONDS)
+                readTimeout(30, TimeUnit.SECONDS)
+                writeTimeout(30, TimeUnit.SECONDS)
+            }
+        }
+    }
+
+    private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
         }
     }
 
