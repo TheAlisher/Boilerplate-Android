bbe3137757a778b87cb2941380f7b6cd94ec21c4
macbookpro <abenov2005.uzer@gmail.com>
2021-03-14 04:55:27 +0600

    removed unnecessary

--- app/src/main/java/com/alis/boilerplate/models/ExampleData.kt
+++ app/src/main/java/com/alis/boilerplate/models/ExampleData.kt
@@ -1,9 +1,7 @@
 package com.alis.boilerplate.models
 
-import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "data")
 data class ExampleData(
     @PrimaryKey
     val id: Long,
