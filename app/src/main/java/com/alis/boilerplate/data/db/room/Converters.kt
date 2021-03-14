5c96bb4ead499e6ca8bd8dc0b3f0e6de3bcef1f5
macbookpro <abenov2005.uzer@gmail.com>
2021-03-14 09:53:34 +0600

    removed unnecessary

--- app/src/main/java/com/alis/boilerplate/data/db/room/Converters.kt
+++ app/src/main/java/com/alis/boilerplate/data/db/room/Converters.kt
@@ -4,7 +4,6 @@ import androidx.room.TypeConverter
 import com.alis.boilerplate.models.room.SubRoomEntity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import java.sql.Date
 
 class Converters {
 
@@ -16,16 +15,6 @@ class Converters {
 
     private inline fun <reified T> toJson(generic: T?): String? {
         return Gson().toJson(generic, type<T>())
-    }
-
-    @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return value?.let { Date(it) }
-    }
-
-    @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time
     }
 
     @TypeConverter
