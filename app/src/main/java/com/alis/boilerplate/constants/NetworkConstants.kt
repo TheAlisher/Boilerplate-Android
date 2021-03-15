ab07e3f0f7966f3aa4344a5c06053b334f898a0e
macbookpro <abenov2005.uzer@gmail.com>
2021-03-16 04:19:59 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/constants/NetworkConstants.kt
+++ app/src/main/java/com/alis/boilerplate/constants/NetworkConstants.kt
@@ -2,6 +2,6 @@ package com.alis.boilerplate.constants
 
 object NetworkConstants {
 
-    const val BASE_URL = "https://example.org/api/"
-    const val HOST = "example.org/api"
+    const val BASE_URL = "https://example.org/"
+    const val HOST = "example.org"
 }
