7807f13e3b166c69c19278790a81561f6350af09
macbookpro <abenov2005.uzer@gmail.com>
2021-03-20 00:35:07 +0600

    added -> overrideOnBackPressed method

--- app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
@@ -1,6 +1,8 @@
 package com.alis.boilerplate.extensions
 
 import android.view.View
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 
@@ -10,4 +12,10 @@ fun View.navigate(actionID: Int) {
 
 fun View.navigate(directions: NavDirections) {
     setOnClickListener(Navigation.createNavigateOnClickListener(directions))
+}
+
+fun Fragment.overrideOnBackPressed(onBackPressed: () -> Unit) {
+    requireActivity().onBackPressedDispatcher.addCallback(this) {
+        onBackPressed()
+    }
 }
