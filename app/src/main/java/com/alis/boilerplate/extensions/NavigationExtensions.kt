80e68d53bfe19dc52cb6c2a40a81cd4aabccd2c8
macbookpro <abenov2005.uzer@gmail.com>
2021-03-22 03:57:57 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
@@ -8,8 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 
-fun View.navigate(@IdRes actionID: Int) {
-    setOnClickListener(Navigation.createNavigateOnClickListener(actionID))
+fun View.navigate(@IdRes actionId: Int) {
+    setOnClickListener(Navigation.createNavigateOnClickListener(actionId))
 }
 
 fun View.navigate(directions: NavDirections) {
