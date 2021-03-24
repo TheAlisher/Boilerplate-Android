61b7db5a993b96b53b6e36a8522da70e445a7ecd
macbookpro <abenov2005.uzer@gmail.com>
2021-03-25 00:49:49 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/models/ExampleData.kt
+++ app/src/main/java/com/alis/boilerplate/models/ExampleData.kt
@@ -1,9 +1,6 @@
 package com.alis.boilerplate.models
 
-import androidx.room.PrimaryKey
-
 data class ExampleData(
-    @PrimaryKey
     val id: Long,
     val title: String,
     val description: String?
