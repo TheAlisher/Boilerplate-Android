32fab3a287148c277aa8e97e5ccea672d717107d
macbookpro <abenov2005.uzer@gmail.com>
2021-03-24 19:07:52 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/base/BaseFragment.kt
+++ app/src/main/java/com/alis/boilerplate/base/BaseFragment.kt
@@ -6,12 +6,12 @@ import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<Binding : ViewBinding, ViewModel : BaseViewModel>(
+abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewBinding>(
     @LayoutRes layoutId: Int
 ) : Fragment(layoutId) {
 
-    protected abstract val binding: Binding
     protected abstract val viewModel: ViewModel
+    protected abstract val binding: Binding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
