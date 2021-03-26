a2b8af2d1f68583e73bf88407efe599ef8b4fa71
macbookpro <abenov2005.uzer@gmail.com>
2021-03-26 06:18:09 +0600

    added -> navigateSafely

--- app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/alis/boilerplate/extensions/NavigationExtensions.kt
@@ -1,5 +1,6 @@
 package com.alis.boilerplate.extensions
 
+import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
@@ -8,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+
+val NavController.previousDestination
+    get() = previousBackStackEntry?.destination
 
 fun View.navigate(@IdRes actionId: Int) {
     setOnClickListener(Navigation.createNavigateOnClickListener(actionId))
@@ -23,5 +27,22 @@ fun Fragment.overrideOnBackPressed(onBackPressed: OnBackPressedCallback. () -> U
     }
 }
 
-val NavController.previousDestination
-    get() = previousBackStackEntry?.destination
+fun NavController.navigateSafely(@IdRes actionId: Int) {
+    try {
+        navigate(actionId)
+    } catch (E: IllegalArgumentException) {
+        loggingNavigateSafely(E)
+    }
+}
+
+fun NavController.navigateSafely(directions: NavDirections) {
+    try {
+        navigate(directions)
+    } catch (E: IllegalArgumentException) {
+        loggingNavigateSafely(E)
+    }
+}
+
+private fun loggingNavigateSafely(E: IllegalArgumentException) {
+    Log.wtf("NavigateSafely", "This is a normal exception, do not pay attention", E)
+}
