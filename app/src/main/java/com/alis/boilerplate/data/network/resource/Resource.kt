72fc07cdce216a15e25ae0b98579850077e2f2d1
macbookpro <abenov2005.uzer@gmail.com>
2021-05-12 23:04:19 +0600

    minor changes

--- app/src/main/java/com/alis/boilerplate/data/network/resource/Resource.kt
+++ app/src/main/java/com/alis/boilerplate/data/network/resource/Resource.kt
@@ -3,9 +3,8 @@ package com.alis.boilerplate.data.network.resource
 sealed class Resource<T>(
     val data: T? = null,
     val message: String? = null,
-    val isLoading: Boolean = false
 ) {
-    class Loading<T>(data: T? = null) : Resource<T>(data = data, isLoading = true)
+    class Loading<T>(data: T? = null) : Resource<T>(data = data)
     class Success<T>(data: T) : Resource<T>(data = data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data = data, message = message)
 }
