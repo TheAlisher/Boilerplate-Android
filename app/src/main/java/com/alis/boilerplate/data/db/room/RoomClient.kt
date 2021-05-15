6cae4be550837ad2d585074c3f7277d1569c17ae
macbookpro <abenov2005.uzer@gmail.com>
2021-05-16 02:13:20 +0600

    renamed database from boilerplate.db to database-boilerplate

--- app/src/main/java/com/alis/boilerplate/data/db/room/RoomClient.kt
+++ app/src/main/java/com/alis/boilerplate/data/db/room/RoomClient.kt
@@ -7,7 +7,7 @@ import com.alis.boilerplate.data.db.room.daos.RoomFooDao
 class RoomClient {
 
     fun provideRoom(context: Context) = Room
-        .databaseBuilder(context, RoomDatabase::class.java, "boilerplate.db")
+        .databaseBuilder(context, RoomDatabase::class.java, "database-boilerplate")
         .build()
 
     fun provideRoomFooDao(roomDatabase: RoomDatabase): RoomFooDao = roomDatabase
