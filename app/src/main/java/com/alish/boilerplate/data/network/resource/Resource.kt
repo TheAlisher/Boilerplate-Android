f0dbb93ecf3c81b44c133b27302b4175781b53d7
macbookpro <abenov2005.uzer@gmail.com>
2021-05-19 02:31:12 +0600

    minor changes

--- app/src/main/java/com/alish/boilerplate/data/network/resource/Resource.kt
+++ app/src/main/java/com/alish/boilerplate/data/network/resource/Resource.kt
@@ -5,6 +5,6 @@ sealed class Resource<out T>(
     val message: String? = null,
 ) {
     class Loading<T>(data: T? = null) : Resource<T>(data = data)
-    class Success<T>(data: T) : Resource<T>(data = data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data = data, message = message)
+    class Success<T>(data: T) : Resource<T>(data = data)
 }
