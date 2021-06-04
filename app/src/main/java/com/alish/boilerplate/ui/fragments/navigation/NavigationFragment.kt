663d9fcfa8dcdd6ce82d490462dfcbaf7e839928
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-05 01:51:50 +0600

    moved Navigation from Activity to Fragment

---
+++ app/src/main/java/com/alish/boilerplate/ui/fragments/navigation/NavigationFragment.kt
@@ -0,0 +1,29 @@
+package com.alish.boilerplate.ui.fragments.navigation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.alish.boilerplate.R
+
+class NavigationFragment : Fragment(R.layout.fragment_navigation) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            parentFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            updateUIComponents(destination.id)
+        }
+    }
+
+    private fun updateUIComponents(id: Int) {
+        // …
+    }
+}
