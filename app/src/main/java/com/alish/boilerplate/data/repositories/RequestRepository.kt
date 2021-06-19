967fa40272db39c6f93832c4684074d4ad637b28
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-20 03:31:42 +0600

    refactoring

--- app/src/main/java/com/alish/boilerplate/data/repositories/RequestRepository.kt
+++ app/src/main/java/com/alish/boilerplate/data/repositories/RequestRepository.kt
@@ -1,16 +1,13 @@
 package com.alish.boilerplate.data.repositories
 
-import androidx.lifecycle.LiveData
 import com.alish.boilerplate.base.BaseRepository
-import com.alish.boilerplate.data.network.resource.Resource
 import com.alish.boilerplate.data.network.retrofit.apiservices.FooApiService
-import com.alish.boilerplate.models.Foo
 
 class RequestRepository(
     private val service: FooApiService
 ) : BaseRepository() {
 
-    fun fetchFoo(): LiveData<Resource<Foo>> {
-        return doRequest { service.fetchFoo() }
+    fun fetchFoo() = doRequest {
+        service.fetchFoo()
     }
 }
