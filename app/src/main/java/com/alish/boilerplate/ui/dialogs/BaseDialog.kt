616e45ccf39c72d73e9b9c79a88330317a47e62d
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-26 00:41:50 +0600

    refactoring

--- app/src/main/java/com/alish/boilerplate/ui/dialogs/BaseDialog.kt
+++ app/src/main/java/com/alish/boilerplate/ui/dialogs/BaseDialog.kt
@@ -5,15 +5,15 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatDialogFragment
-import com.alish.boilerplate.databinding.DialogExampleBinding
+import com.alish.boilerplate.databinding.DialogBaseBinding
 
-class ExampleDialog : AppCompatDialogFragment() {
+class BaseDialog : AppCompatDialogFragment() {
 
-    private var _binding: DialogExampleBinding? = null
+    private var _binding: DialogBaseBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        _binding = DialogExampleBinding.inflate(LayoutInflater.from(context))
+        _binding = DialogBaseBinding.inflate(LayoutInflater.from(context))
         val builder = AlertDialog.Builder(activity)
             .setView(binding.root)
             .create()
