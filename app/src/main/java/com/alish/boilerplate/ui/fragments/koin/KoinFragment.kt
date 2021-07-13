46e55c5785603d4e26d56f93af1885514036471d
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-07-13 23:53:12 +0600

    removed layout

--- app/src/main/java/com/alish/boilerplate/ui/fragments/koin/KoinFragment.kt
+++ app/src/main/java/com/alish/boilerplate/ui/fragments/koin/KoinFragment.kt
@@ -1,10 +1,9 @@
 package com.alish.boilerplate.ui.fragments.koin
 
 import androidx.fragment.app.Fragment
-import com.alish.boilerplate.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class KoinFragment : Fragment(R.layout.fragment_koin) {
+class KoinFragment : Fragment() {
 
     private val viewModel: KoinViewModel by viewModel()
 }
