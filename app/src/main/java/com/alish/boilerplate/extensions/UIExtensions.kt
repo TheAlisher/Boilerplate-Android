f953a6aff3691677ece54fdc9c6730ec5155b1d0
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-07-16 03:28:00 +0600

    added @LayoutRes and change parameter name from textFromResource to textFromRes in showToast

--- app/src/main/java/com/alish/boilerplate/extensions/UIExtensions.kt
+++ app/src/main/java/com/alish/boilerplate/extensions/UIExtensions.kt
@@ -3,6 +3,7 @@ package com.alish.boilerplate.extensions
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.alish.boilerplate.data.resource.Resource
@@ -18,16 +19,16 @@ fun Fragment.showToastShort(text: String) {
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 }
 
-fun Fragment.showToastShort(textFromResource: Int) {
-    Toast.makeText(context, textFromResource, Toast.LENGTH_SHORT).show()
+fun Fragment.showToastShort(@StringRes textFromRes: Int) {
+    Toast.makeText(context, textFromRes, Toast.LENGTH_SHORT).show()
 }
 
 fun Fragment.showToastLong(text: String) {
     Toast.makeText(context, text, Toast.LENGTH_LONG).show()
 }
 
-fun Fragment.showToastLong(textFromResource: Int) {
-    Toast.makeText(context, textFromResource, Toast.LENGTH_LONG).show()
+fun Fragment.showToastLong(@StringRes textFromRes: Int) {
+    Toast.makeText(context, textFromRes, Toast.LENGTH_LONG).show()
 }
 
 // endregion
