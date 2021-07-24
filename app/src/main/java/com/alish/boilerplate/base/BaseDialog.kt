aa85b774fc14516cf02209f563ec565c74ed743f
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-07-25 00:53:13 +0600

    Initial

---
+++ app/src/main/java/com/alish/boilerplate/base/BaseDialog.kt
@@ -0,0 +1,39 @@
+package com.alish.boilerplate.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseDialog<Binding : ViewBinding> : AppCompatDialogFragment() {
+
+    protected abstract val binding: Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        /**
+         * for yogacp View Binding Property Delegate library
+         */
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialize()
+        setupArgs()
+        setupListeners()
+    }
+
+    open fun initialize() {
+    }
+
+    open fun setupArgs() {
+    }
+
+    open fun setupListeners() {
+    }
+}
