90d1f8984e5be98646bff0114b6c78b26282bda0
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-07-26 00:28:52 +0600

    Initial

---
+++ app/src/main/java/com/alish/boilerplate/base/BaseFlowFragment.kt
@@ -0,0 +1,29 @@
+package com.alish.boilerplate.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+abstract class BaseFlowFragment(
+    @LayoutRes layoutId: Int,
+    @IdRes private val navHostFragmentId: Int
+) : Fragment(layoutId) {
+
+    protected lateinit var navController: NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupNavigation()
+    }
+
+    protected fun setupNavigation() {
+        val navHostFragment =
+            childFragmentManager.findFragmentById(navHostFragmentId) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
