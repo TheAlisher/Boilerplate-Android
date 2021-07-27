40b0b3c27191d4f5ce579192e7455221f30c3f90
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-07-27 13:08:24 +0600

    Initial

---
+++ app/src/main/java/com/alish/boilerplate/extensions/JSONExtensions.kt
@@ -0,0 +1,18 @@
+package com.alish.boilerplate.extensions
+
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+private fun String.isValidJSON(): Boolean {
+    try {
+        JSONObject(this)
+    } catch (exception: JSONException) {
+        try {
+            JSONArray(this)
+        } catch (exception: JSONException) {
+            return false
+        }
+    }
+    return true
+}
