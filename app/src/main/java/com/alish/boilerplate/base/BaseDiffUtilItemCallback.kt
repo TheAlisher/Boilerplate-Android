81a3a66b216b066097acfb8a44dfc8cd060c8f31
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-18 04:29:29 +0600

    added generic for id property

--- app/src/main/java/com/alish/boilerplate/base/BaseDiffUtilItemCallback.kt
+++ app/src/main/java/com/alish/boilerplate/base/BaseDiffUtilItemCallback.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.DiffUtil
  * Base Comparator
  */
 
-class BaseDiffUtilItemCallback<T : IBaseDiffModel> : DiffUtil.ItemCallback<T>() {
+class BaseDiffUtilItemCallback<N : Number, T : IBaseDiffModel<N>> : DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
         return oldItem.id == newItem.id
--- app/src/main/java/com/alish/boilerplate/domain/models/Foo.kt
+++ app/src/main/java/com/alish/boilerplate/domain/models/Foo.kt
@@ -5,4 +5,4 @@ import com.alish.boilerplate.base.IBaseDiffModel
 data class Foo(
     override val id: Long,
     val bar: String
-) : IBaseDiffModel
+) : IBaseDiffModel<Long>
