549fa230b8ab014b4398c3742fdab8ade65c0921
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-20 02:28:23 +0600

    refactoring

--- app/src/main/java/com/alish/boilerplate/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/alish/boilerplate/data/local/db/AppDatabase.kt
@@ -2,12 +2,10 @@ package com.alish.boilerplate.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.alish.boilerplate.data.local.db.daos.FooDao
 import com.alish.boilerplate.data.local.db.entities.FooEntity
 
 @Database(entities = [FooEntity::class], version = 1)
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun fooDao(): FooDao
