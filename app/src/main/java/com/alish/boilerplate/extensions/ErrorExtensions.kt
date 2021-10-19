02b32959dcdb78fe3b26cd989719551f9db0ea0f
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-20 01:14:33 +0600

    removed unnecessary

--- app/src/main/java/com/alish/boilerplate/extensions/ErrorExtensions.kt
+++ app/src/main/java/com/alish/boilerplate/extensions/ErrorExtensions.kt
@@ -1,14 +1,8 @@
 package com.alish.boilerplate.extensions
 
 import androidx.core.widget.doOnTextChanged
-import androidx.fragment.app.Fragment
-import com.alish.boilerplate.common.resource.Resource
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-
-fun <T> Resource.Error<T>.showError(fragment: Fragment) {
-    message?.let { error -> fragment.showToastLong(error) }
-}
 
 fun TextInputEditText.disableErrorWhenStartWrite(inputLayout: TextInputLayout) {
     doOnTextChanged { _, _, _, _ ->
