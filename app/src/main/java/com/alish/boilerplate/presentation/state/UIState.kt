6667703c66cd5854a3fa2d7047df13411c2ea790
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-21 19:06:15 +0600

    Initial

---
+++ app/src/main/java/com/alish/boilerplate/presentation/state/UIState.kt
@@ -0,0 +1,7 @@
+package com.alish.boilerplate.presentation.state
+
+sealed class UIState<T> {
+    class Loading<T> : UIState<T>()
+    class Error<T>(val error: String) : UIState<T>()
+    class Success<T>(val data: T) : UIState<T>()
+}
