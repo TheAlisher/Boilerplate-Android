af1990ecf834bd2c4dca10f3a8affe8b91349198
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-23 02:59:14 +0600

    removed unnecessary

--- app/src/main/java/com/alish/boilerplate/common/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/alish/boilerplate/common/extensions/NavigationExtensions.kt
@@ -7,10 +7,6 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
-import androidx.navigation.findNavController
-import com.alish.boilerplate.R
-
-fun Fragment.mainNavController() = requireActivity().findNavController(R.id.nav_host_fragment)
 
 fun NavController.navigateSafely(@IdRes actionId: Int) {
     try {
