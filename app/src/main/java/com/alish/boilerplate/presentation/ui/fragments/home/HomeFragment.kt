bd81a1f9d8948ac10113702983ea42cffaa68a2f
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-10-23 02:55:32 +0600

    removed unnecessary

--- app/src/main/java/com/alish/boilerplate/presentation/ui/fragments/home/HomeFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.alish.boilerplate.presentation.ui.fragments.home
-
-import androidx.fragment.app.Fragment
-import com.alish.boilerplate.R
-
-class HomeFragment : Fragment(R.layout.fragment_home) {
-    // …
-}
