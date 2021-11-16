d10996c6b15a6d2ad632fd62102ab6fdd5bfb280
TheAlisher <allisher.abenov@gmail.com>
2021-11-16 19:15:03 +0600

    updated Kotlin version from 1.5.31 to 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
 
         // Navigation Safe Args
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Dependencies.Versions.nav}")
