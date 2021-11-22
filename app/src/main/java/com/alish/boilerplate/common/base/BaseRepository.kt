b666ade6a90fa66d04e493881500762d64b4e6ed
TheAlisher <allisher.abenov@gmail.com>
2021-11-23 05:12:25 +0600

    added doPagingRequest method

--- app/src/main/java/com/alish/boilerplate/common/base/BaseRepository.kt
+++ app/src/main/java/com/alish/boilerplate/common/base/BaseRepository.kt
@@ -1,6 +1,10 @@
 package com.alish.boilerplate.common.base
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.alish.boilerplate.common.resource.Resource
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 abstract class BaseRepository {
@@ -16,5 +20,29 @@ abstract class BaseRepository {
                 )
             )
         }
+    }
+
+    protected fun <ValueDto : Any, Value : Any> doPagingRequest(
+        pagingSource: BasePagingSource<ValueDto, Value>,
+        pageSize: Int = 10,
+        prefetchDistance: Int = pageSize,
+        enablePlaceholders: Boolean = true,
+        initialLoadSize: Int = pageSize * 3,
+        maxSize: Int = Int.MAX_VALUE,
+        jumpThreshold: Int = Int.MIN_VALUE
+    ): Flow<PagingData<Value>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize,
+                prefetchDistance,
+                enablePlaceholders,
+                initialLoadSize,
+                maxSize,
+                jumpThreshold
+            ),
+            pagingSourceFactory = {
+                pagingSource
+            }
+        ).flow
     }
 }
--- app/src/main/java/com/alish/boilerplate/data/repositories/FooRepositoryImpl.kt
+++ app/src/main/java/com/alish/boilerplate/data/repositories/FooRepositoryImpl.kt
@@ -1,15 +1,10 @@
 package com.alish.boilerplate.data.repositories
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import com.alish.boilerplate.common.base.BaseRepository
 import com.alish.boilerplate.data.network.apiservices.FooApiService
 import com.alish.boilerplate.data.network.dtos.toFoo
 import com.alish.boilerplate.data.network.pagingsources.FooPagingSource
-import com.alish.boilerplate.domain.models.Foo
 import com.alish.boilerplate.domain.repositories.FooRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class FooRepositoryImpl @Inject constructor(
@@ -20,13 +15,5 @@ class FooRepositoryImpl @Inject constructor(
         service.fetchFoo().toFoo()
     }
 
-    fun fetchFooPaging(): Flow<PagingData<Foo>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = 10
-            )
-        ) {
-            FooPagingSource(service)
-        }.flow
-    }
+    fun fetchFooPaging() = doPagingRequest(FooPagingSource(service))
 }
