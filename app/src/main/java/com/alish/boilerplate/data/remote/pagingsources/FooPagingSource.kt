edea08e3455e8e7ff4e3841cb1ba1e35d8ef4a37
TheAlisher <allisher.abenov@gmail.com>
2021-12-21 04:03:52 +0600

    minor changes

--- app/src/main/java/com/alish/boilerplate/data/remote/pagingsources/FooPagingSource.kt
+++ app/src/main/java/com/alish/boilerplate/data/remote/pagingsources/FooPagingSource.kt
@@ -1,9 +1,9 @@
 package com.alish.boilerplate.data.remote.pagingsources
 
-import com.alish.boilerplate.common.base.BasePagingSource
 import com.alish.boilerplate.data.remote.apiservices.FooApiService
 import com.alish.boilerplate.data.remote.dtos.FooDto
 import com.alish.boilerplate.data.remote.dtos.toDomain
+import com.alish.boilerplate.data.remote.pagingsources.base.BasePagingSource
 import com.alish.boilerplate.domain.models.Foo
 
 class FooPagingSource(
