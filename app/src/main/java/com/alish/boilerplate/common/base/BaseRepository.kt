f9f005bf2a6837e4f89c030dfbd9b755da1be149
TheAlisher <allisher.abenov@gmail.com>
2021-12-21 02:49:56 +0600

    minor changes

--- app/src/main/java/com/alish/boilerplate/common/base/BaseRepository.kt
+++ app/src/main/java/com/alish/boilerplate/common/base/BaseRepository.kt
@@ -4,21 +4,25 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.alish.boilerplate.common.resource.Resource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 
 abstract class BaseRepository {
 
     protected fun <T> doRequest(request: suspend () -> T) = flow {
-        emit(Resource.Loading())
-        try {
-            emit(Resource.Success(data = request()))
-        } catch (ioException: Exception) {
-            emit(
-                Resource.Error(
-                    data = null, message = ioException.localizedMessage ?: "Error Occurred!"
+        withContext(Dispatchers.IO) {
+            emit(Resource.Loading())
+            try {
+                emit(Resource.Success(data = request()))
+            } catch (ioException: Exception) {
+                emit(
+                    Resource.Error(
+                        data = null, message = ioException.localizedMessage ?: "Error Occurred!"
+                    )
                 )
-            )
+            }
         }
     }
 
