3a5d58f827254f52a9b76a8d2ab9a8f70cd0751e
TheAlisher <allisher.abenov@gmail.com>
2022-01-13 10:26:35 +0600

    updated OkHttp version from 4.9.0 to 5.0.0-alpha.3

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
     // OkHttp
-    implementation("com.squareup.okhttp3:okhttp-bom:4.9.0")
+    implementation("com.squareup.okhttp3:okhttp-bom:5.0.0-alpha.3")
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
