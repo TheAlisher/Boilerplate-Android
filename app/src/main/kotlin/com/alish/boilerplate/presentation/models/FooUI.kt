5235715bb8129995241f6a31f8c46159bf91e635
TheAlisher <allisher.abenov@gmail.com>
2022-01-24 13:21:14 +0600

    minor changes

--- app/src/main/kotlin/com/alish/boilerplate/presentation/models/FooUI.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/models/FooUI.kt
@@ -1,6 +1,7 @@
 package com.alish.boilerplate.presentation.models
 
 import com.alish.boilerplate.domain.models.Foo
+import com.alish.boilerplate.presentation.base.IBaseDiffModel
 
 data class FooUI(
     override val id: Long,
