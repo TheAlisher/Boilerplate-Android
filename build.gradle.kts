083ca6eb94f3c22605e4f3f68f57eb17016f3a79
TheAlisher <allisher.abenov@gmail.com>
2022-01-28 15:30:44 +0600

    updated Gradle version from 7.0.4 to 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven { url = uri("https://jitpack.io") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
         // Navigation Safe Args
