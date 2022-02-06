ba778d260113b398aa2ac310ac5461a1b1a00aa6
TheAlisher <allisher.abenov@gmail.com>
2022-02-06 15:58:43 +0600

    added pluginManagement & dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,23 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+rootProject.name = "Boilerplate-Android"
 include(
     ":app",
     ":data",
     ":domain",
     ":common"
 )
-rootProject.name = "Boilerplate-Android"
