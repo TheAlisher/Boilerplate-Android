d8b66c74d7f50ad6a146f8ec6a3259d968d1d791
TheAlisher <allisher.abenov@gmail.com>
2022-02-07 15:35:31 +0600

    added Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
-    kotlin("android") version "1.6.10" apply false
+    kotlin("android") version Dependencies.Versions.kotlin apply false
+
+    // Dokka
+    id("org.jetbrains.dokka") version Dependencies.Versions.kotlin
 
     // Navigation Safe Args
     id("androidx.navigation.safeargs.kotlin") version Dependencies.Versions.nav apply false
@@ -9,6 +12,10 @@ plugins {
 
 buildscript {
     dependencies {
+
+        // Dokka
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${Dependencies.Versions.kotlin}")
+
         // Hilt
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Dependencies.Versions.hilt}")
     }
