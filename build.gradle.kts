18534f032f65a29bcd9a6bd3e2efb8d377c277e4
TheAlisher <allisher.abenov@gmail.com>
2022-02-07 16:12:26 +0600

    removed Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,12 @@ plugins {
     id("com.android.library") version "7.1.1" apply false
     kotlin("android") version Dependencies.Versions.kotlin apply false
 
-    // Dokka
-    id("org.jetbrains.dokka") version Dependencies.Versions.kotlin
-
     // Navigation Safe Args
     id("androidx.navigation.safeargs.kotlin") version Dependencies.Versions.nav apply false
 }
 
 buildscript {
     dependencies {
-
-        // Dokka
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${Dependencies.Versions.kotlin}")
 
         // Hilt
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Dependencies.Versions.hilt}")
