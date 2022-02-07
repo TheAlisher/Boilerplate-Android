c0c6d26efdca0e930c667d5d391d98b00aa55d70
TheAlisher <allisher.abenov@gmail.com>
2022-02-07 15:35:20 +0600

    added Kotlin versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,13 @@
 object Dependencies {
 
     object Versions {
+
+        const val kotlin = "1.6.10"
+
+        // Navigation
         const val nav = "2.5.0-alpha01"
+
+        // Hilt
         const val hilt = "2.40.5"
     }
 }
