ab9414005cd1793bef1ce18dabee691f54812171
TheAlisher <allisher.abenov@gmail.com>
2022-02-14 03:10:09 +0600

    minor changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
     kotlin("android") version "1.6.10" apply false
-
-    // Navigation Safe Args
-    id("androidx.navigation.safeargs.kotlin") version Dependencies.Versions.nav apply false
 }
 
 buildscript {
     dependencies {
+
+        // Navigation Safe Args
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Dependencies.Versions.nav}")
 
         // Hilt
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Dependencies.Versions.hilt}")
