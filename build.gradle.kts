c809c0a77dac5bf3df364d84e846e1691c4bb3a5
TheAlisher <allisher.abenov@gmail.com>
2022-02-14 04:44:59 +0600

    minor changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
-    kotlin("android") version Dependencies.Versions.kotlin apply false
+    kotlin("android") version "1.6.10" apply false
 }
 
 buildscript {
