65e34b8ad6836d68ebff65d53d7da5af0c7c9235
TheAlisher <allisher.abenov@gmail.com>
2022-02-15 01:54:54 +0600

    refactored Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,9 +69,7 @@ dependencies {
     implementation("com.google.android.material:material:1.5.0")
 
     // UI Components
-    // | ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    // | ViewBindingPropertyDelegate | To use only without reflection variants of viewBinding
     implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.3")
 
     // Activity
