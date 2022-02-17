a34a03fbac46a5e994f2bcbbe5fa2467b726f061
TheAlisher <allisher.abenov@gmail.com>
2022-02-17 10:28:34 +0600

    updated Hilt version from 2.40.5 to 2.41 and minor changes in Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,10 @@ plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
     kotlin("android") version "1.6.10" apply false
-}
 
-buildscript {
-    dependencies {
+    // Navigation Safe Args
+    id(Dependencies.Navigation.safeArgsPlugin) version Dependencies.Navigation.version apply false
 
-        // Navigation Safe Args
-        classpath(Dependencies.Navigation.safeArgsGradlePlugin)
-
-        // Hilt
-        classpath(Dependencies.Hilt.androidGradlePlugin)
-    }
+    // Hilt
+    id(Dependencies.Hilt.plugin) version Dependencies.Hilt.version apply false
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -36,22 +36,19 @@ object Dependencies {
     }
 
     object Navigation {
-        private const val version = "2.4.1"
+        const val version = "2.4.1"
 
         const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
         const val ui = "androidx.navigation:navigation-ui-ktx:$version"
-
-        const val safeArgsGradlePlugin =
-            "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
+        const val safeArgsPlugin = "androidx.navigation.safeargs.kotlin"
     }
 
     object Hilt {
-        private const val version = "2.40.5"
+        const val version = "2.41"
 
         const val android = "com.google.dagger:hilt-android:$version"
         const val kapt = "com.google.dagger:hilt-compiler:$version"
-
-        const val androidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val plugin = "com.google.dagger.hilt.android"
     }
 
     // Data
