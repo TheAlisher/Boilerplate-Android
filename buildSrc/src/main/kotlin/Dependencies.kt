331f0f2b25f90bed3f0f1cbfda1a16179968b334
TheAlisher <allisher.abenov@gmail.com>
2022-02-24 15:18:27 +0600

    updated Room version from 2.4.1 to 2.4.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -77,7 +77,7 @@ object Dependencies {
     }
 
     object Room {
-        private const val version = "2.4.1"
+        private const val version = "2.4.2"
 
         const val runtime = "androidx.room:room-runtime:$version"
         const val compiler = "androidx.room:room-compiler:$version"
