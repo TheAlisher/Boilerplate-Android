d9f38f2f5f9f8f664c8a1215761c7b105222c341
TheAlisher <allisher.abenov@gmail.com>
2022-03-11 05:57:22 +0600

    refactoring Network Module, Client, Builder

--- app/src/main/kotlin/com/alish/boilerplate/di/NetworkModule.kt
+++ app/src/main/kotlin/com/alish/boilerplate/di/NetworkModule.kt
@@ -1,11 +1,16 @@
 package com.alish.boilerplate.di
 
 import androidx.lifecycle.MutableLiveData
+import com.alish.boilerplate.data.remote.NetworkClient
+import com.alish.boilerplate.data.remote.OkHttp
 import com.alish.boilerplate.data.remote.RetrofitClient
+import com.alish.boilerplate.data.remote.apiservices.AuthenticatorApiService
+import com.alish.boilerplate.data.remote.authenticator.TokenAuthenticator
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -18,12 +23,13 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideAuthenticatorApiService() = RetrofitClient.Authenticator()
-        .provideAuthenticatorApiService()
+    fun provideAuthenticatorApiService(
+        authenticatorClient: NetworkClient.AuthenticatorClient
+    ) = authenticatorClient.provideAuthenticatorApiService()
 
     @Singleton
     @Provides
     fun provideFooApiService(
-        retrofitClient: RetrofitClient
-    ) = retrofitClient.provideFooApiService()
+        networkClient: NetworkClient
+    ) = networkClient.provideFooApiService()
 }
---
+++ data/src/main/kotlin/com/alish/boilerplate/data/remote/NetworkClient.kt
@@ -0,0 +1,35 @@
+package com.alish.boilerplate.data.remote
+
+import com.alish.boilerplate.data.remote.apiservices.AuthenticatorApiService
+import com.alish.boilerplate.data.remote.apiservices.FooApiService
+import com.alish.boilerplate.data.remote.authenticator.TokenAuthenticator
+import javax.inject.Inject
+
+class NetworkClient @Inject constructor(
+    retrofitClient: RetrofitClient,
+    okHttp: OkHttp,
+    authenticator: TokenAuthenticator
+) {
+
+    private val provideRetrofit = retrofitClient.provideRetrofit(
+        okHttp.provideOkHttpClient(authenticator)
+    )
+
+    fun provideFooApiService() = provideRetrofit.create(
+        FooApiService::class.java
+    )
+
+    class AuthenticatorClient @Inject constructor(
+        retrofitClient: RetrofitClient,
+        okHttp: OkHttp,
+    ) {
+
+        private val provideRetrofit = retrofitClient.provideRetrofit(
+            okHttp.provideOkHttpClient(null)
+        )
+
+        fun provideAuthenticatorApiService() = provideRetrofit.create(
+            AuthenticatorApiService::class.java
+        )
+    }
+}
