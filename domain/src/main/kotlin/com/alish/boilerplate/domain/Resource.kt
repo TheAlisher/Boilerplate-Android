4d78dddffa46c3d4ce4b222ea51a47d14a12fb49
TheAlisher <allisher.abenov@gmail.com>
2022-04-05 11:50:14 +0600

    Removed Resource.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/Resource.kt
+++
@@ -1,9 +0,0 @@
-package com.alish.boilerplate.domain
-
-sealed class Resource<out T>(
-    val data: T? = null,
-    val message: String? = null,
-) {
-    class Error<T>(message: String) : Resource<T>(message = message)
-    class Success<T>(data: T) : Resource<T>(data = data)
-}
