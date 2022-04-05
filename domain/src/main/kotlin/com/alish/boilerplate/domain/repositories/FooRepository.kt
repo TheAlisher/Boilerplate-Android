91a45c5eca74fa823f288aa46ffadc31d55241ca
TheAlisher <allisher.abenov@gmail.com>
2022-04-05 12:02:31 +0600

    minor changes

--- domain/src/main/kotlin/com/alish/boilerplate/domain/repositories/FooRepository.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/repositories/FooRepository.kt
@@ -1,10 +1,10 @@
 package com.alish.boilerplate.domain.repositories
 
-import com.alish.boilerplate.domain.Resource
+import com.alish.boilerplate.domain.Either
 import com.alish.boilerplate.domain.models.Foo
 import kotlinx.coroutines.flow.Flow
 
 interface FooRepository {
 
-    fun fetchFoo(): Flow<Resource<Foo>>
+    fun fetchFoo(): Flow<Either<Foo>>
 }
