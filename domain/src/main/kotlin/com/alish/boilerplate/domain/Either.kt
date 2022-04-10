751ce9f33e75095a77af484b483bdb2cbdb9f429
TheAlisher <allisher.abenov@gmail.com>
2022-04-11 02:38:07 +0600

    Initial Either.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/Either.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/Either.kt
@@ -1,6 +1,6 @@
 package com.alish.boilerplate.domain
 
-sealed class Either<out R> {
-    class Left(val error: String) : Either<Nothing>()
-    class Right<out T>(val data: T) : Either<T>()
+sealed class Either<out A, out B> {
+    class Left<out A>(val value: A) : Either<A, Nothing>()
+    class Right<out B>(val value: B) : Either<Nothing, B>()
 }
