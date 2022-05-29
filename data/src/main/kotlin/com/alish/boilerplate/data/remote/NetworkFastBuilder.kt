1242d7bae31c640b49ba0bbb5cb386b2d36813de
TheAlisher <allisher.abenov@gmail.com>
2022-05-29 21:57:17 +0600

    minor refactored

--- data/src/main/kotlin/com/alish/boilerplate/data/remote/NetworkFastBuilder.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/remote/NetworkFastBuilder.kt
@@ -17,15 +17,13 @@ internal fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Bu
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
-internal fun provideOkHttpClientBuilder(): OkHttpClient.Builder {
-    return OkHttpClient()
-        .newBuilder()
-        .addInterceptor(provideLoggingInterceptor())
-        .callTimeout(30, TimeUnit.SECONDS)
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-}
+internal fun provideOkHttpClientBuilder(): OkHttpClient.Builder = OkHttpClient()
+    .newBuilder()
+    .addInterceptor(provideLoggingInterceptor())
+    .callTimeout(30, TimeUnit.SECONDS)
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
+    .writeTimeout(30, TimeUnit.SECONDS)
 
 private fun provideLoggingInterceptor() = HttpLoggingInterceptor().setLevel(
     when {
