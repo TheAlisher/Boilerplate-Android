1a7d0645c1ef6adee29e517c742edf4b4a4cb940
TheAlisher <allisher.abenov@gmail.com>
2022-05-29 16:55:54 +0600

    Refactored repository request function: - Removed BaseRepository.kt - Added RepositoryCore.kt - Refactored doRequest function

--- data/src/main/kotlin/com/alish/boilerplate/data/repositories/FooRepositoryImpl.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/repositories/FooRepositoryImpl.kt
@@ -1,26 +1,24 @@
 package com.alish.boilerplate.data.repositories
 
-import com.alish.boilerplate.data.repositories.base.BaseRepository
 import com.alish.boilerplate.data.remote.apiservices.FooApiService
 import com.alish.boilerplate.data.remote.dtos.foo.toDomain
 import com.alish.boilerplate.data.remote.pagingsources.FooPagingSource
-import com.alish.boilerplate.domain.models.Foo
+import com.alish.boilerplate.data.repositories.core.doPagingRequest
+import com.alish.boilerplate.data.repositories.core.doRequest
 import com.alish.boilerplate.domain.repositories.FooRepository
+import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class FooRepositoryImpl @Inject constructor(
     private val service: FooApiService
-) : BaseRepository(), FooRepository {
+) : FooRepository {
 
-    override fun fetchFoo() = doRequest(this::setupFooSuccess) {
-        service.fetchFoo().toDomain()
-    }
-
-    /**
-     * Insert data to database or save to SharedPreferences / DataStore
-     */
-    private fun setupFooSuccess(foo: Foo) {
-        foo.id
+    override fun fetchFoo() = doRequest {
+        service.fetchFoo().also {
+            it.bar
+        }.toDomain()
+    }.catch { exception ->
+        exception.message
     }
 
     fun fetchFooPaging() = doPagingRequest(FooPagingSource(service))
