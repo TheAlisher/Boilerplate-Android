47b0f327ec2d2afbcd3953489bf76788de3e4780
TheAlisher <allisher.abenov@gmail.com>
2022-06-18 05:35:51 +0600

    added functions: - activityNavController() - flowNavController()

--- app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/NavigationExtensions.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/NavigationExtensions.kt
@@ -6,7 +6,15 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.alish.boilerplate.R
+
+// fun Fragment.activityNavController() = requireActivity().findNavController(R.id.nav_host_fragment)
+
+fun Fragment.flowNavController(@IdRes navHostId: Int) = requireActivity().findNavController(
+    navHostId
+)
 
 fun NavController.navigateSafely(@IdRes actionId: Int) {
     currentDestination?.getAction(actionId)?.let { navigate(actionId) }
