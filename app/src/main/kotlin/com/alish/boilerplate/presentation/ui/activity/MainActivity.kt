5de334bac9856cf7b860f99485d75fab6943986d
TheAlisher <allisher.abenov@gmail.com>
2022-07-28 13:37:26 +0600

    added AndroidEntryPoint to MainActivity.kt

--- app/src/main/kotlin/com/alish/boilerplate/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/ui/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.alish.boilerplate.presentation.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alish.boilerplate.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
