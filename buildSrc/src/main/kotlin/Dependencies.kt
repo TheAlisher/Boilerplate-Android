5eb0ca1b22b005af78cad4f28f704612b2c9a528
TheAlisher <allisher.abenov@gmail.com>
2022-09-22 11:41:30 +0600

    updated Activity version to 1.6.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val constraintLayout = "2.1.4"
     const val vbpd = "1.5.6"
     const val core = "1.9.0"
-    const val activity = "1.5.1"
+    const val activity = "1.6.0"
     const val fragment = "1.5.2"
     const val lifecycle = "2.5.1"
     const val navigation = "2.5.2"
