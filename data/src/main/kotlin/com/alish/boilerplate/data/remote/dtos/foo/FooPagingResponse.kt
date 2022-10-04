e124bf38ef026013ec94576ad817d6a9ed7edf01
TheAlisher <allisher.abenov@gmail.com>
2022-10-04 18:24:30 +0600

    minor refactored

--- data/src/main/kotlin/com/alish/boilerplate/data/remote/dtos/foo/FooPagingResponse.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/remote/dtos/foo/FooPagingResponse.kt
@@ -1,7 +1,12 @@
 package com.alish.boilerplate.data.remote.dtos.foo
 
-data class FooPagingResponse<T>(
+import com.google.gson.annotations.SerializedName
+
+class FooPagingResponse<T>(
+    @SerializedName("prev")
     val prev: Int?,
+    @SerializedName("next")
     val next: Int?,
+    @SerializedName("data")
     val data: MutableList<T>
 )
