62ca3349fac240ccb5c76ef78c61a826c478f886
TheAlisher <allisher.abenov@gmail.com>
2022-11-06 15:53:55 +0600

    minor refactoring

--- data/src/main/kotlin/com/alish/boilerplate/data/local/db/AppDatabase.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/local/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.alish.boilerplate.data.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.alish.boilerplate.data.local.db.daos.FooDao
-import com.alish.boilerplate.data.local.db.entities.FooEntity
+import com.alish.boilerplate.data.local.db.entities.foo.FooEntity
 
 @Database(entities = [FooEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
