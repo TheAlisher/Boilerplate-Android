a52b2e50746c505fc1e08ab9ed0aae1b876e6f8b
TheAlisher <allisher.abenov@gmail.com>
2022-11-14 14:23:39 +0600

    added RemotePagingWrapper

--- domain/src/main/kotlin/com/alish/boilerplate/domain/repositories/FooRepository.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/repositories/FooRepository.kt
@@ -2,6 +2,7 @@ package com.alish.boilerplate.domain.repositories
 
 import androidx.paging.PagingData
 import com.alish.boilerplate.domain.models.foo.Foo
+import com.alish.boilerplate.domain.utils.RemotePagingWrapper
 import com.alish.boilerplate.domain.utils.RemoteWrapper
 import kotlinx.coroutines.flow.Flow
 
@@ -9,5 +10,5 @@ interface FooRepository {
 
     fun fetchFoo(): RemoteWrapper<Foo>
 
-    fun fetchFooPaging(): Flow<PagingData<Foo>>
+    fun fetchFooPaging(): RemotePagingWrapper<Foo>
 }
--- domain/src/main/kotlin/com/alish/boilerplate/domain/utils/TypeAliases.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/utils/TypeAliases.kt
@@ -1,5 +1,8 @@
 package com.alish.boilerplate.domain.utils
 
+import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 
 internal typealias RemoteWrapper<T> = Flow<Either<NetworkError, T>>
+
+internal typealias RemotePagingWrapper<T> = Flow<PagingData<T>>
