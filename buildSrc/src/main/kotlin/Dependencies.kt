d583a0c0339bb8aac473fa812d871c5e1a370957
TheAlisher <allisher.abenov@gmail.com>
2023-01-25 16:16:42 +0600

    updated Material Design Components version to 1.8.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
     const val kotlin = "1.8.0"
     const val coroutines = "1.6.4"
     const val KSP = "1.8.0-1.0.8"
-    const val material = "1.7.0"
+    const val material = "1.8.0"
     const val constraintLayout = "2.1.4"
     const val vbpd = "1.5.6"
     const val core = "1.9.0"
