d7b5bf3e79198f21c305d6b3faf5749bf3efb4ca
TheAlisher <allisher.abenov@gmail.com>
2023-02-10 11:39:04 +0600

    updated Dagger version to 2.45

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     const val fragment = "1.5.5"
     const val lifecycle = "2.5.1"
     const val navigation = "2.5.3"
-    const val dagger = "2.44.2"
+    const val dagger = "2.45"
     const val retrofit = "2.9.0"
     const val okHttp = "5.0.0-alpha.11"
     const val room = "2.5.0"
