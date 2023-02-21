9e6dec602252a6501992d2121c5a37b4de5e6636
TheAlisher <allisher.abenov@gmail.com>
2023-02-22 01:17:23 +0600

    added KDoc in BaseDiffUtil.kt

--- app/src/main/kotlin/com/alish/boilerplate/presentation/base/BaseDiffUtil.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/base/BaseDiffUtil.kt
@@ -2,11 +2,23 @@ package com.alish.boilerplate.presentation.base
 
 import androidx.recyclerview.widget.DiffUtil
 
+/**
+ * Interface for [BaseDiffUtilItemCallback]
+ *
+ * @see equals
+ */
 interface IBaseDiffModel<T> {
     val id: T
     override fun equals(other: Any?): Boolean
 }
 
+/**
+ * Base class for fast create [DiffUtil] in
+ * [ListAdapter][androidx.recyclerview.widget.ListAdapter], [PagingAdapter][androidx.paging.PagingDataAdapter]
+ *
+ * @see IBaseDiffModel
+ * @see DiffUtil.ItemCallback
+ */
 class BaseDiffUtilItemCallback<T : IBaseDiffModel<S>, S> : DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
