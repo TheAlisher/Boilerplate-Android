39f1cf37c62cd71b3b1912a6b900006ce128ec11
TheAlisher <allisher.abenov@gmail.com>
2023-02-21 19:59:42 +0600

    added KDoc for Either.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/utils/Either.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/utils/Either.kt
@@ -1,5 +1,10 @@
 package com.alish.boilerplate.domain.utils
 
+/**
+ * Either is used to short-circuit a computation upon the first error.
+ *
+ * By convention, the right side of an Either is used to hold successful values.
+ */
 sealed class Either<out A, out B> {
     class Left<out A>(val value: A) : Either<A, Nothing>()
     class Right<out B>(val value: B) : Either<Nothing, B>()
