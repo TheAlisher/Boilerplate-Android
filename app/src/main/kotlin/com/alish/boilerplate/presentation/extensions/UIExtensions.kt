986046e01c19f64f01aea38d8071d6b612fb8fee
TheAlisher <allisher.abenov@gmail.com>
2023-02-21 23:25:40 +0600

    added hideKeyboard extension

--- app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/UIExtensions.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/UIExtensions.kt
@@ -1,5 +1,7 @@
 package com.alish.boilerplate.presentation.extensions
 
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
@@ -8,7 +10,7 @@ import androidx.fragment.app.Fragment
 // region Fragment
 
 /**
- * Fragment Extensions
+ * Fragment extensions
  */
 
 fun Fragment.showToastShort(text: String) {
@@ -25,6 +27,13 @@ fun Fragment.showToastLong(text: String) {
 
 fun Fragment.showToastLong(@StringRes textFromRes: Int) {
     Toast.makeText(context, textFromRes, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.hideKeyboard() {
+    val inputMethodManager = requireActivity().getSystemService(
+        Activity.INPUT_METHOD_SERVICE
+    ) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(requireView().windowToken, 0)
 }
 
 // endregion
