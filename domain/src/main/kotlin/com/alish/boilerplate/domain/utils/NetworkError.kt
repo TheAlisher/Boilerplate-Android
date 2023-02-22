f664cd3caa4b7dc4a9b733c71dd1db449dfc8074
TheAlisher <allisher.abenov@gmail.com>
2023-02-22 19:09:40 +0600

    added KDoc in NetworkError.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/utils/NetworkError.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/utils/NetworkError.kt
@@ -1,7 +1,28 @@
 package com.alish.boilerplate.domain.utils
 
+/**
+ * Network error wrapper class
+ *
+ * @author Alish
+ */
 sealed class NetworkError {
+
+    /**
+     * Unexpected error for example 500 or exception when mapping data
+     */
     class Unexpected(val error: String) : NetworkError()
+
+    /**
+     * Default errors from server side
+     */
     class Api(val error: String) : NetworkError()
+
+    /**
+     * Errors for display in inputs
+     *
+     * @param error
+     * Map [Key][kotlin.collections.Map.Entry.key] is input name,
+     * Map [Value][kotlin.collections.Map.Entry.value] is errors from server side
+     */
     class ApiInputs(val error: MutableMap<String, List<String>>) : NetworkError()
 }
