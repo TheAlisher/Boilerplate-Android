b0be84a6fc09e917611cc4c58242dd38f30bf6de
TheAlisher <allisher.abenov@gmail.com>
2023-02-22 19:12:20 +0600

    added KDoc in TypeAliases.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/utils/TypeAliases.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/utils/TypeAliases.kt
@@ -3,6 +3,19 @@ package com.alish.boilerplate.domain.utils
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Simple wrapper for convenience of network requests in repositories
+ *
+ * @see Flow
+ * @see Either
+ * @see NetworkError
+ */
 internal typealias RemoteWrapper<T> = Flow<Either<NetworkError, T>>
 
+/**
+ * Simple wrapper for convenience of network paging requests in repositories
+ *
+ * @see Flow
+ * @see PagingData
+ */
 internal typealias RemotePagingWrapper<T> = Flow<PagingData<T>>
