4d74ff0d24a17106f594d27e14db34d75ba3ccc8
TheAlisher <allisher.abenov@gmail.com>
2023-02-22 19:32:58 +0600

    minor changes

--- data/src/main/kotlin/com/alish/boilerplate/data/utils/DataMapper.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/utils/DataMapper.kt
@@ -1,5 +1,14 @@
 package com.alish.boilerplate.data.utils
 
+/**
+ * Base mapper class
+ *
+ * @param T domain layer model
+ */
 interface DataMapper<T> {
+
+    /**
+     * Map DTO to domain layer model
+     */
     fun mapToDomain(): T
 }
