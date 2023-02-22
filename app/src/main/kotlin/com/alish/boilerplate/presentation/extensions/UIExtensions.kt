76071d8e4ff38c2383dcdf9d0d5f4ca8bafc3df3
TheAlisher <allisher.abenov@gmail.com>
2023-02-22 20:54:07 +0600

    added KDoc

--- app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/UIExtensions.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/extensions/UIExtensions.kt
@@ -7,28 +7,49 @@ import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 
-// region Fragment
+// region Fragment extensions
 
 /**
- * Fragment extensions
+ * Fast show [Toast]
+ *
+ * @receiver [Fragment]
  */
-
 fun Fragment.showToastShort(text: String) {
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 }
 
+/**
+ * Fast show [Toast]
+ *
+ * @receiver [Fragment]
+ */
 fun Fragment.showToastShort(@StringRes textFromRes: Int) {
     Toast.makeText(context, textFromRes, Toast.LENGTH_SHORT).show()
 }
 
+/**
+ * Fast show [Toast]
+ *
+ * @receiver [Fragment]
+ */
 fun Fragment.showToastLong(text: String) {
     Toast.makeText(context, text, Toast.LENGTH_LONG).show()
 }
 
+/**
+ * Fast show [Toast]
+ *
+ * @receiver [Fragment]
+ */
 fun Fragment.showToastLong(@StringRes textFromRes: Int) {
     Toast.makeText(context, textFromRes, Toast.LENGTH_LONG).show()
 }
 
+/**
+ * Hide keyboard extension function
+ *
+ * @receiver [Fragment]
+ */
 fun Fragment.hideKeyboard() {
     val inputMethodManager = requireActivity().getSystemService(
         Activity.INPUT_METHOD_SERVICE
