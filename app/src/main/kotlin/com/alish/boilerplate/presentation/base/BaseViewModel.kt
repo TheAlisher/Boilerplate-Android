29b7d8231fa34cbed7fb5b25a9c082dd416a3ea9
TheAlisher <allisher.abenov@gmail.com>
2023-02-24 11:55:37 +0600

    added protected for MutableUIStateFlow functions

--- app/src/main/kotlin/com/alish/boilerplate/presentation/base/BaseViewModel.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/base/BaseViewModel.kt
@@ -25,12 +25,12 @@ abstract class BaseViewModel : ViewModel() {
      * Creates [MutableStateFlow] with [UIState] and the given initial value [UIState.Idle]
      */
     @Suppress("FunctionName")
-    fun <T> MutableUIStateFlow() = MutableStateFlow<UIState<T>>(UIState.Idle())
+    protected fun <T> MutableUIStateFlow() = MutableStateFlow<UIState<T>>(UIState.Idle())
 
     /**
      * Reset [MutableUIStateFlow] to [UIState.Idle]
      */
-    fun <T> MutableStateFlow<UIState<T>>.reset() {
+    protected fun <T> MutableStateFlow<UIState<T>>.reset() {
         value = UIState.Idle()
     }
 
