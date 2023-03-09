f7183fff61456b9bc76762c0d676e176022eefc5
TheAlisher <allisher.abenov@gmail.com>
2023-03-09 19:17:49 +0600

    added Room ktx dependency for Kotlin Extensions and Coroutines support for Room

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -87,6 +87,7 @@ object Libraries {
     object Room {
         const val runtime = "androidx.room:room-runtime:${Versions.room}"
         const val compiler = "androidx.room:room-compiler:${Versions.room}"
+        const val ktx = "androidx.room:room-ktx:${Versions.room}"
     }
 
     object Paging {
