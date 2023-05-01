94b9da2aa9be6003ba019606bbe76a46d733ad15
TheAlisher <allisher.abenov@gmail.com>
2023-05-02 04:55:48 +0600

    updated Core version from 1.9.0 to 1.10.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val constraintLayout = "2.1.4"
     const val vbpd = "1.5.9"
     const val splashscreen = "1.0.1"
-    const val core = "1.9.0"
+    const val core = "1.10.0"
     const val activity = "1.7.1"
     const val fragment = "1.5.7"
     const val lifecycle = "2.6.1"
