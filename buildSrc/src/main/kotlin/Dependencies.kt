9f67175129b7f4c5e8af505390f88254a5365943
TheAlisher <allisher.abenov@gmail.com>
2023-06-11 23:48:55 +0600

    updated Fragment version from 1.5.7 to 1.6.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -10,7 +10,7 @@ object Versions {
     const val core = "1.10.1"
     const val splashscreen = "1.0.1"
     const val activity = "1.7.2"
-    const val fragment = "1.5.7"
+    const val fragment = "1.6.0"
     const val lifecycle = "2.6.1"
     const val navigation = "2.5.3"
     const val dagger = "2.46.1"
