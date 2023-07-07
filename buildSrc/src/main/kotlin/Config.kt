a4aa95843d403b562a1267ba559209d372c624fd
TheAlisher <allisher.abenov@gmail.com>
2023-07-07 14:47:24 +0600

    added Options to Config.kt

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion
+
 object AndroidConfig {
 
     const val compileSdk = 33
@@ -12,4 +14,10 @@ object Namespaces {
 
     const val app = "com.alish.boilerplate"
     const val data = "com.alish.boilerplate.data"
+}
+
+object Options {
+
+    val compileOptions = JavaVersion.VERSION_17
+    const val kotlinOptions = "17"
 }
