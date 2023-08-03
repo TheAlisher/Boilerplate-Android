91e9ffb9241802977c39b828e52700061271e9b9
TheAlisher <allisher.abenov@gmail.com>
2023-08-03 07:13:18 +0600

    updated KSP version from 1.9.0-1.0.12 to 1.9.0-1.0.13

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val AGP = "8.1.0"
     const val kotlin = "1.9.0"
     const val coroutines = "1.7.3"
-    const val KSP = "1.9.0-1.0.12"
+    const val KSP = "1.9.0-1.0.13"
     const val material = "1.9.0"
     const val constraintLayout = "2.1.4"
     const val vbpd = "1.5.9"
