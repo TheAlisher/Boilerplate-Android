2a0d02ef134aefef507ee986e56acc723525ca5e
TheAlisher <allisher.abenov@gmail.com>
2023-12-25 23:56:58 +0600

    renamed RoomClient to DatabaseClient and changed some implementation for DI

--- data/src/main/kotlin/com/alish/boilerplate/data/local/db/DatabaseClient.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/local/db/DatabaseClient.kt
@@ -3,12 +3,17 @@ package com.alish.boilerplate.data.local.db
 import android.content.Context
 import androidx.room.Room
 import com.alish.boilerplate.data.local.db.daos.FooDao
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class RoomClient {
+@Singleton
+class DatabaseClient @Inject constructor(
+    context: Context
+) {
 
-    fun provideAppDatabase(context: Context) = Room
+    private val provideAppDatabase = Room
         .databaseBuilder(context, AppDatabase::class.java, "boilerplate.db")
         .build()
 
-    fun provideFooDao(appDatabase: AppDatabase): FooDao = appDatabase.fooDao()
+    fun provideFooDao(): FooDao = provideAppDatabase.fooDao()
 }
