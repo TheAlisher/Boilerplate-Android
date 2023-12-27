a009784d27a4adbd0a112a3e39faecf86d63bb46
TheAlisher <allisher.abenov@gmail.com>
2023-12-28 01:38:48 +0600

    removed PreferencesModule.kt and add Singleton and Inject to PreferencesHelper.kt

--- data/src/main/kotlin/com/alish/boilerplate/data/local/preferences/PreferencesHelper.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/local/preferences/PreferencesHelper.kt
@@ -2,11 +2,17 @@ package com.alish.boilerplate.data.local.preferences
 
 import android.content.Context
 import android.content.SharedPreferences
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class PreferencesHelper(context: Context) {
+@Singleton
+class PreferencesHelper @Inject constructor(
+    context: Context
+) {
 
-    private val preferences: SharedPreferences =
-        context.getSharedPreferences("boilerplate.preferences", Context.MODE_PRIVATE)
+    private val preferences: SharedPreferences = context.getSharedPreferences(
+        "boilerplate.preferences", Context.MODE_PRIVATE
+    )
 
     private fun remove(key: String) {
         preferences.edit().remove(key).apply()
