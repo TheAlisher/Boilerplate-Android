901bfe95a974976c2599f32482a6163beff83f3e
TheAlisher <allisher.abenov@gmail.com>
2024-01-23 01:36:01 +0600

    added EncryptedSharedPreferences

--- data/src/main/kotlin/com/alish/boilerplate/data/local/preferences/PreferencesHelper.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/local/preferences/PreferencesHelper.kt
@@ -1,20 +1,31 @@
 package com.alish.boilerplate.data.local.preferences
 
 import android.content.Context
-import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class PreferencesHelper @Inject constructor(
-    context: Context
+	context: Context,
 ) {
 
-    private val preferences: SharedPreferences = context.getSharedPreferences(
-        "boilerplate.preferences", Context.MODE_PRIVATE
-    )
+	private val masterKey: MasterKey = MasterKey.Builder(context)
+		.setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+		.build()
 
-    private fun remove(key: String) {
-        preferences.edit().remove(key).apply()
-    }
+	private val preferences = EncryptedSharedPreferences.create(
+		context,
+		"boilerplate.preferences",
+		masterKey,
+		EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+		EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+	)
+
+	private val editor = preferences.edit()
+
+	private fun remove(key: String) {
+		preferences.edit().remove(key).apply()
+	}
 }
