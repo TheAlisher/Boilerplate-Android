eeb07a097e13399c910453ef8b40577634fa8f01
TheAlisher <allisher.abenov@gmail.com>
2024-01-23 02:02:20 +0600

    added UserData

--- data/src/main/kotlin/com/alish/boilerplate/data/remote/client/interceptors/AuthorizationInterceptor.kt
+++ data/src/main/kotlin/com/alish/boilerplate/data/remote/client/interceptors/AuthorizationInterceptor.kt
@@ -1,19 +1,19 @@
 package com.alish.boilerplate.data.remote.client.interceptors
 
-import com.alish.boilerplate.data.local.preferences.PreferencesHelper
+import com.alish.boilerplate.data.local.preferences.UserDataPreferences
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
 class AuthorizationInterceptor @Inject constructor(
-    private val preferencesHelper: PreferencesHelper
+    private val userData: UserDataPreferences,
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain
             .request()
             .newBuilder()
-            .addHeader("Authorization", "$preferencesHelper.getAccessToken()")
+            .addHeader("Authorization", userData.accessToken)
             .build()
         return chain.proceed(request)
     }
