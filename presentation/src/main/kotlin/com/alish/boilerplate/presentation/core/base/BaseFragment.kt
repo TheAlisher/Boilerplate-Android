cfd4735a0f6af259d0f6e2c270ec379bba36f490
TheAlisher <allisher.abenov@gmail.com>
2024-01-29 04:10:53 +0600

    minor refactored

--- presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/base/BaseFragment.kt
+++ presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/base/BaseFragment.kt
@@ -7,18 +7,16 @@ import androidx.constraintlayout.widget.Group
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.PagingData
 import androidx.viewbinding.ViewBinding
 import com.alish.boilerplate.domain.core.NetworkError
 import com.alish.boilerplate.presentation.core.extensions.showToastLong
 import com.alish.boilerplate.presentation.core.UIState
+import com.alish.boilerplate.presentation.core.extensions.collectSafely
+import com.alish.boilerplate.presentation.core.extensions.launchRepeatOnLifecycle
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.android.material.textfield.TextInputLayout
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 
 /**
  * Base class for [Fragment]s that work with data
@@ -68,17 +66,13 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewBinding>(
         onError: ((error: NetworkError) -> Unit),
         onSuccess: ((data: T) -> Unit),
     ) {
-        launchRepeatOnLifecycle(lifecycleState) {
-            this@collectUIState.collect {
-                state?.invoke(it)
-                when (it) {
-                    is UIState.Idle -> {}
-
-                    is UIState.Loading -> {}
-
-                    is UIState.Error -> onError.invoke(it.error)
-                    is UIState.Success -> onSuccess.invoke(it.data)
-                }
+        this.collectSafely(viewLifecycleOwner, lifecycleState) {
+            state?.invoke(it)
+            when (it) {
+                is UIState.Idle -> {}
+                is UIState.Loading -> {}
+                is UIState.Error -> onError.invoke(it.error)
+                is UIState.Success -> onSuccess.invoke(it.data)
             }
         }
     }
@@ -92,7 +86,9 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewBinding>(
         lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
         action: suspend (value: PagingData<T>) -> Unit,
     ) {
-        launchRepeatOnLifecycle(lifecycleState) { this@collectPaging.collectLatest { action(it) } }
+        viewLifecycleOwner.launchRepeatOnLifecycle(
+            lifecycleState
+        ) { this@collectPaging.collectLatest { action(it) } }
     }
 
     /**
@@ -112,7 +108,6 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewBinding>(
 
         when (this) {
             is UIState.Idle -> {}
-
             is UIState.Loading -> showLoader(true)
             is UIState.Error -> showLoader(false)
             is UIState.Success -> showLoader(!willShowViewIfSuccess)
@@ -148,39 +143,6 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewBinding>(
 
         is NetworkError.Timeout -> {
             showToastLong("Timeout")
-        }
-    }
-
-    /**
-     * Collect flow safely with [launchRepeatOnLifecycle]
-     */
-    protected fun <T> Flow<T>.collectSafely(
-        state: Lifecycle.State = Lifecycle.State.STARTED,
-        collector: (T) -> Unit,
-    ) {
-        launchRepeatOnLifecycle(state) {
-            this@collectSafely.collect {
-                collector(it)
-            }
-        }
-    }
-
-    /**
-     * Launch coroutine with [repeatOnLifecycle] API
-     *
-     * @param state [Lifecycle.State][androidx.lifecycle.Lifecycle.State] in which `block` runs in a new coroutine. That coroutine
-     * will cancel if the lifecycle falls below that state, and will restart if it's in that state
-     * again.
-     * @param block The block to run when the lifecycle is at least in [state] state.
-     */
-    private fun launchRepeatOnLifecycle(
-        state: Lifecycle.State,
-        block: suspend CoroutineScope.() -> Unit,
-    ) {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(state) {
-                block()
-            }
         }
     }
 }
