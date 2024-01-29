823290512b264775331434a8a342c818973fac09
TheAlisher <allisher.abenov@gmail.com>
2024-01-29 20:16:37 +0600

    minor changes

--- presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/extensions/AsynchronousExtensions.kt
+++ presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/extensions/AsynchronousExtensions.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 /**
- * [AsynchronousExtensions] = only for docs and links
+ * [AsynchronousExtensions] - only for docs and links
  *
  * Must read:
  * [repeatOnLifecycle API design story](https://medium.com/androiddevelopers/repeatonlifecycle-api-design-story-8670d1a7d333)
