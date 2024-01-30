7bee6a92e75b01340541dd8f4553962693bd2160
TheAlisher <allisher.abenov@gmail.com>
2024-01-31 01:35:07 +0600

    added docs for UIState.kt

--- app/src/main/kotlin/com/alish/boilerplate/presentation/core/UIState.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/core/UIState.kt
@@ -1,10 +1,39 @@
 package com.alish.boilerplate.presentation.core
 
+import com.alish.boilerplate.domain.core.Either
 import com.alish.boilerplate.domain.core.NetworkError
 
+/**
+ * The [UIState] class represents the screen state in response to various actions,
+ * such as requesting data, awaiting a response, handling errors, and successfully completing a request with data.
+ *
+ * @param T The type of data associated with the state.
+ * @see [Either]
+ */
 sealed class UIState<T> {
+
+    /**
+     * [Idle] - The default state when there are no active data requests, and the screen has just been opened.
+     */
     class Idle<T> : UIState<T>()
+
+    /**
+     * [Loading] - The state after sending a data request and waiting for a response.
+     */
     class Loading<T> : UIState<T>()
+
+    /**
+     * [Error] - The state in case of an error occurring during a data request.
+     *
+     * @param error The network error associated with the current state.
+     * @see NetworkError
+     */
     class Error<T>(val error: NetworkError) : UIState<T>()
+
+    /**
+     * [Success] - The state when a data request is successful, and data is returned.
+     *
+     * @param data The data returned as a result of a successful request.
+     */
     class Success<T>(val data: T) : UIState<T>()
 }
