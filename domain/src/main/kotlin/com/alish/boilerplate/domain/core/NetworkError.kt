993c78d3eed87037a9b1ba4d99b87bf98e56cd0a
TheAlisher <allisher.abenov@gmail.com>
2024-01-31 01:43:27 +0600

    minor refactoring in NetworkError.kt

--- domain/src/main/kotlin/com/alish/boilerplate/domain/core/NetworkError.kt
+++ domain/src/main/kotlin/com/alish/boilerplate/domain/core/NetworkError.kt
@@ -8,26 +8,26 @@ package com.alish.boilerplate.domain.core
 sealed class NetworkError {
 
     /**
+     * State for Timeout exceptions
+     */
+    data object Timeout : NetworkError()
+
+    /**
      * State for unexpected exceptions, for example «HTTP code - 500» or exceptions when mapping models
      */
-    class Unexpected(val error: String) : NetworkError()
+    class Unexpected(val errorMessage: String) : NetworkError()
 
     /**
      * State for default errors from server size
      */
-    class Api(val error: String?) : NetworkError()
+    class Api(val errorMessage: String?) : NetworkError()
 
     /**
      * State for displaying errors in input fields
      *
-     * @param error
+     * @param inputErrors
      * Map [Key][kotlin.collections.Map.Entry.key] is input name,
      * Map [Value][kotlin.collections.Map.Entry.value] is errors from server side
      */
-    class ApiInputs(val error: MutableMap<String, List<String>>?) : NetworkError()
-
-    /**
-     * State for Timeout exceptions
-     */
-    data object Timeout : NetworkError()
+    class ApiInputs(val inputErrors: MutableMap<String, List<String>>?) : NetworkError()
 }
