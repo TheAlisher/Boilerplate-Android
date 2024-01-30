c328c1fd935a22c8aaa0c1e22fedab7b7025f0ad
TheAlisher <allisher.abenov@gmail.com>
2024-01-30 23:13:29 +0600

    changed retrofit & room implementation to api

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
     implementation(project(":domain"))
 
     // Retrofit
-    implementation(Libraries.Retrofit.retrofit)
-    implementation(Libraries.Retrofit.converterMoshi)
+    api(Libraries.Retrofit.retrofit)
+    api(Libraries.Retrofit.converterMoshi)
 
     // Moshi
     implementation(Libraries.Moshi.moshi)
@@ -36,9 +36,9 @@ dependencies {
     implementation(Libraries.OkHttp.loggingInterceptor)
 
     // Room
-    implementation(Libraries.Room.runtime)
+    api(Libraries.Room.runtime)
     ksp(Libraries.Room.compiler)
-    implementation(Libraries.Room.ktx)
+    api(Libraries.Room.ktx)
 
     // Security
     implementation(Libraries.Security.crypto)
