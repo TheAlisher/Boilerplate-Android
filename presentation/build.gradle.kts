cb84d63b4c85a2a438363a07a686598fd02dbca0
TheAlisher <allisher.abenov@gmail.com>
2024-01-31 10:46:29 +0600

    moved presentation dependencies from app module to presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,6 +2,9 @@ import extensions.Namespaces
 
 plugins {
     id(Plugins.Conventions.androidLibrary)
+
+    // Navigation Safe Args
+    id(Plugins.Navigation.safeArgs)
 }
 
 android {
@@ -14,4 +17,34 @@ android {
 
 dependencies {
 
+    implementation(project(":domain"))
+
+    // Kotlin
+    implementation(Libraries.Coroutines.android)
+
+    // UI Components
+    implementation(Libraries.UIComponents.material)
+    implementation(Libraries.UIComponents.constraintLayout)
+    implementation(Libraries.UIComponents.vbpd)
+
+    // Core
+    implementation(Libraries.Core.core)
+    implementation(Libraries.Core.splashscreen)
+
+    // Activity
+    implementation(Libraries.Activity.activity)
+
+    // Fragment
+    implementation(Libraries.Fragment.fragment)
+
+    // Lifecycle
+    implementation(Libraries.Lifecycle.viewModel)
+    implementation(Libraries.Lifecycle.runtime)
+
+    // Navigation
+    implementation(Libraries.Navigation.fragment)
+    implementation(Libraries.Navigation.ui)
+
+    // Paging
+    implementation(Libraries.Paging.runtime)
 }
