528700c6992dcb826cc812ec5928f127f86e1e1f
TheAlisher <allisher.abenov@gmail.com>
2024-01-31 10:54:12 +0600

    moved activity_main.xml from app module to presentation module and added setup graph in MainActivity.kt

--- app/src/main/kotlin/com/alish/boilerplate/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/alish/boilerplate/presentation/ui/activity/MainActivity.kt
@@ -3,15 +3,27 @@ package com.alish.boilerplate.presentation.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.alish.boilerplate.R
+import com.alish.boilerplate.presentation.R.id.nav_host_fragment
+import com.alish.boilerplate.presentation.R.layout.activity_main
+import com.alish.boilerplate.R.navigation.nav_graph
+import com.alish.boilerplate.presentation.core.extensions.initNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy { initNavController(nav_host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navGraph = navController.navInflater.inflate(nav_graph)
+        navController.graph = navGraph
     }
 }
