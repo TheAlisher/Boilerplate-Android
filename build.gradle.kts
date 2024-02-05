a4c0ffe02c4655d0d7dba1e95635857b21a7daf7
TheAlisher <allisher.abenov@gmail.com>
2024-02-05 10:13:32 +0600

    reformat code

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
     id(Plugins.AGP.application) version Versions.AGP apply false
     kotlin(Plugins.Kotlin.android) version Versions.kotlin apply false
 
-    // KSP
-    id(Plugins.KSP.ksp) version Versions.KSP apply false
-
     // Navigation Safe Args
     id(Plugins.Navigation.safeArgs) version Versions.navigation apply false
 
     // Hilt
     id(Plugins.Hilt.android) version Versions.dagger apply false
+
+    // KSP
+    id(Plugins.KSP.ksp) version Versions.KSP apply false
 }
