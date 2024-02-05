acb86e78602e481fbeefc81f70433a4d461ab43c
TheAlisher <allisher.abenov@gmail.com>
2024-02-05 10:12:52 +0600

    removed unnecessary AGP library plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id(Plugins.AGP.application) version Versions.AGP apply false
-    id(Plugins.AGP.library) version Versions.AGP apply false
     kotlin(Plugins.Kotlin.android) version Versions.kotlin apply false
 
     // KSP
