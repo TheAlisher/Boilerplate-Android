82a0190be601e5ac7dcba8fef463fd6a133b60e4
TheAlisher <allisher.abenov@gmail.com>
2024-02-09 16:48:57 +0600

    added namespaces `feature` & `featureFoo`

--- build-logic/src/main/kotlin/extensions/ConventionExtensions.kt
+++ build-logic/src/main/kotlin/extensions/ConventionExtensions.kt
@@ -32,4 +32,7 @@ object Namespaces {
     const val app = "com.alish.boilerplate"
     const val data = "$app.data"
     const val presentation = "$app.presentation"
+
+    private const val feature = "${app}.feature"
+    const val featureFoo = "${feature}.foo"
 }
