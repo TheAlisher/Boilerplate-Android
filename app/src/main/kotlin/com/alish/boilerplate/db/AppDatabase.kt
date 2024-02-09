8a7441510e07de40bcf33cdda85444f1ef53f221
TheAlisher <allisher.abenov@gmail.com>
2024-02-09 20:07:24 +0600

    added docs for AppDatabase

--- app/src/main/kotlin/com/alish/boilerplate/db/AppDatabase.kt
+++ app/src/main/kotlin/com/alish/boilerplate/db/AppDatabase.kt
@@ -5,6 +5,9 @@ import androidx.room.RoomDatabase
 import com.alish.boilerplate.foo.data.db.daos.FooDao
 import com.alish.boilerplate.foo.data.db.entities.FooEntity
 
+/**
+ * [Must to read](https://issuetracker.google.com/issues/67967869)
+ */
 @Database(entities = [FooEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
