3f074ac746ed4e327a91c4d2f49fc80db4078f46
TheAlisher <allisher.abenov@gmail.com>
2024-02-09 17:51:17 +0600

    added Hilt implementation

--- features/foo/build.gradle.kts
+++ features/foo/build.gradle.kts
@@ -2,8 +2,20 @@ import extensions.Namespaces
 
 plugins {
     id(Plugins.Conventions.androidFeature)
+
+    // Hilt
+    id(Plugins.Hilt.android)
+
+    // KSP
+    id(Plugins.KSP.ksp)
 }
 
 android {
     namespace = Namespaces.featureFoo
+}
+
+dependencies {
+
+    api(Libraries.Hilt.android)
+    ksp(Libraries.Hilt.compiler)
 }
