893da3561ee564cc60151f86232c6ed90d6d106e
TheAlisher <allisher.abenov@gmail.com>
2024-02-15 21:02:11 +0600

    Initial Namespaces.kt

---
+++ buildSrc/src/main/kotlin/Namespaces.kt
@@ -0,0 +1,9 @@
+object Namespaces {
+
+    const val app = "com.alish.boilerplate"
+    const val data = "$app.data"
+    const val presentation = "$app.presentation"
+
+    private const val feature = "$app.feature"
+    const val featureFoo = "$feature.foo"
+}
