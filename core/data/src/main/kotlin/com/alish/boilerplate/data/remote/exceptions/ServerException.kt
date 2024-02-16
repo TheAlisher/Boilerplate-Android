1e31c3a43c7884f1c6bfd5a52b63eb194b650aa3
TheAlisher <allisher.abenov@gmail.com>
2024-02-17 01:15:12 +0600

    removed unnecessary in ServerException

--- core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
+++ core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
@@ -1,21 +1,10 @@
 package com.alish.boilerplate.data.remote.exceptions
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 import java.io.IOException
 
-@JsonClass(generateAdapter = true)
 sealed class ServerException : IOException() {
 
-    @JsonClass(generateAdapter = true)
-    class ApiInputs(
-        @Json(name = "data")
-        val data: Map<String, List<String>>
-    ) : ServerException()
+    class ApiInputs(val data: Map<String, List<String>>) : ServerException()
 
-    @JsonClass(generateAdapter = true)
-    class Api(
-        @Json(name = "message")
-        override val message: String
-    ) : ServerException()
+    class Api(override val message: String) : ServerException()
 }
