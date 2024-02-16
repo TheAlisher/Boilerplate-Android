a04b5a911da5b21914af39898c754612b86da95f
TheAlisher <allisher.abenov@gmail.com>
2024-02-17 01:13:22 +0600

    added Moshi Json annotations

--- core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
+++ core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
@@ -1,10 +1,21 @@
 package com.alish.boilerplate.data.remote.exceptions
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.io.IOException
 
+@JsonClass(generateAdapter = true)
 sealed class ServerException : IOException() {
 
-    class ApiInputs(val data: Map<String, List<String>>) : ServerException()
+    @JsonClass(generateAdapter = true)
+    class ApiInputs(
+        @Json(name = "data")
+        val data: Map<String, List<String>>
+    ) : ServerException()
 
-    class Api(override val message: String) : ServerException()
+    @JsonClass(generateAdapter = true)
+    class Api(
+        @Json(name = "message")
+        override val message: String
+    ) : ServerException()
 }
