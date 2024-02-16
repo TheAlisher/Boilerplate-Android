e594691379db411655c9973a0442d612f9283fe8
TheAlisher <allisher.abenov@gmail.com>
2024-02-16 17:27:23 +0600

    minor changes in ServerException

--- core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
+++ core/data/src/main/kotlin/com/alish/boilerplate/data/remote/exceptions/ServerException.kt
@@ -2,7 +2,7 @@ package com.alish.boilerplate.data.remote.exceptions
 
 sealed class ServerException : Exception() {
 
-    class ApiException(override val message: String) : ServerException()
-
     class ApiInputException(val data: Map<String, List<String>>) : ServerException()
+
+    class ApiException(override val message: String) : ServerException()
 }
