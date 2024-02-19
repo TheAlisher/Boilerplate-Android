8be2d1349b395ed09ce9b2b06e08b049ad099dd3
TheAlisher <allisher.abenov@gmail.com>
2024-02-20 01:10:34 +0600

    removed unnecessary function and imports

--- features/foo/src/main/kotlin/com/alish/boilerplate/foo/data/db/models/FooDBO.kt
+++ features/foo/src/main/kotlin/com/alish/boilerplate/foo/data/db/models/FooDBO.kt
@@ -3,7 +3,6 @@ package com.alish.boilerplate.foo.data.db.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.alish.boilerplate.data.core.utils.DataMapper
-import com.alish.boilerplate.foo.data.network.models.FooDTO
 import com.alish.boilerplate.foo.domain.models.Foo
 
 @Entity(tableName = "foo")
@@ -15,5 +14,3 @@ class FooDBO(
 
     override fun toDomain() = Foo(id, bar)
 }
-
-fun FooDTO.toEntity() = FooDBO(id, bar)
