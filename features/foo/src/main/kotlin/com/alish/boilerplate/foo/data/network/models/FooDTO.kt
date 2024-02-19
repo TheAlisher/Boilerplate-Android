aa1f78590ea5b872a5f43c74f5eb194b2d16db09
TheAlisher <allisher.abenov@gmail.com>
2024-02-20 01:10:20 +0600

    added toDBO mapper function

--- features/foo/src/main/kotlin/com/alish/boilerplate/foo/data/network/models/FooDTO.kt
+++ features/foo/src/main/kotlin/com/alish/boilerplate/foo/data/network/models/FooDTO.kt
@@ -1,6 +1,7 @@
 package com.alish.boilerplate.foo.data.network.models
 
 import com.alish.boilerplate.data.core.utils.DataMapper
+import com.alish.boilerplate.foo.data.db.models.FooDBO
 import com.alish.boilerplate.foo.domain.models.Foo
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -14,4 +15,6 @@ class FooDTO(
 ) : DataMapper<Foo> {
 
     override fun toDomain() = Foo(id, bar)
+
+    fun toDBO() = FooDBO(id, bar)
 }
