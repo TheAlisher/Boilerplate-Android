275d9c8d81d50c34b4cdfc03a30e30b12777779f
TheAlisher <allisher.abenov@gmail.com>
2024-02-21 03:17:55 +0600

    Initial ModuleConfig.kt

---
+++ build-logic/src/main/kotlin/ModuleConfig.kt
@@ -0,0 +1,26 @@
+import org.gradle.api.JavaVersion
+
+object LangOptions {
+
+    const val jvmToolchain: Int = 17
+    val javaVersion: JavaVersion = JavaVersion.VERSION_17
+    const val jvmTarget: String = jvmToolchain.toString()
+}
+
+object AndroidConfig {
+
+    const val applicationId = "com.alish.boilerplate"
+    const val compileSdk: Int = 34
+    const val minSdk: Int = 29
+    const val targetSdk: Int = 34
+}
+
+object Namespaces {
+
+    const val app = AndroidConfig.applicationId
+    const val data = "$app.data"
+    const val presentation = "$app.presentation"
+
+    private const val feature = "$app.feature"
+    const val featureFoo = "$feature.foo"
+}
