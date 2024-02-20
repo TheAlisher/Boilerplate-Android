6f942936d25744d5a83208656d2629f01d0d5c52
TheAlisher <allisher.abenov@gmail.com>
2024-02-21 03:18:11 +0600

    removed unnecessary

--- build-logic/src/main/kotlin/Namespaces.kt
+++
@@ -1,9 +0,0 @@
-object Namespaces {
-
-    const val app = "com.alish.boilerplate"
-    const val data = "$app.data"
-    const val presentation = "$app.presentation"
-
-    private const val feature = "$app.feature"
-    const val featureFoo = "$feature.foo"
-}
