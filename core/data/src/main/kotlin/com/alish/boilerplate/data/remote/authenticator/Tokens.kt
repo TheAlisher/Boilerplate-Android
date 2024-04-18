d12f675e432b8d00e1a5741ac5a0a38789d82f3a
TheAlisher <allisher.abenov@gmail.com>
2024-04-18 14:35:25 +0600

    added Serializable and SerialName to Tokens, RefreshToken

--- core/data/src/main/kotlin/com/alish/boilerplate/data/remote/authenticator/Tokens.kt
+++ core/data/src/main/kotlin/com/alish/boilerplate/data/remote/authenticator/Tokens.kt
@@ -1,10 +1,18 @@
 package com.alish.boilerplate.data.remote.authenticator
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 class Tokens(
+    @SerialName("access")
     val accessToken: String,
+    @SerialName("refresh")
     val refreshToken: String
 )
 
+@Serializable
 class RefreshToken(
+    @SerialName("refresh")
     val refreshToken: String
 )
