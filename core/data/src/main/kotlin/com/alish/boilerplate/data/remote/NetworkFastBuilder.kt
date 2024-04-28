97163d4c0769438173c5e5b95f2dd8c5392c6c3a
TheAlisher <allisher.abenov@gmail.com>
2024-04-28 17:24:26 +0600

    migrate from jake.wharton repository to retrofit2

--- core/data/src/main/kotlin/com/alish/boilerplate/data/remote/NetworkFastBuilder.kt
+++ core/data/src/main/kotlin/com/alish/boilerplate/data/remote/NetworkFastBuilder.kt
@@ -2,18 +2,18 @@ package com.alish.boilerplate.data.remote
 
 import com.alish.boilerplate.data.BuildConfig
 import com.alish.boilerplate.data.core.utils.jsonClient
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.util.concurrent.TimeUnit
 
 internal fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
     .baseUrl(BuildConfig.BASE_URL)
     .client(okHttpClient)
     .addConverterFactory(
-        jsonClient.asConverterFactory("application/json".toMediaType())
+        jsonClient.asConverterFactory("application/json; charset=UTF8".toMediaType())
     )
     .build()
 
