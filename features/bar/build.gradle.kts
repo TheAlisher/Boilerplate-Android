134743fcc3ff88b74c7a135d8dbb5349600259da
TheAlisher <allisher.abenov@gmail.com>
2024-05-01 21:16:42 +0600

    Initial feature Bar

---
+++ features/bar/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.conventions.androidFeature)
+
+    // Kotlinx Serialization
+    alias(libs.plugins.kotlinx.serialization)
+
+    // Navigation Safe Args
+    alias(libs.plugins.androidx.navigation.safeArgs)
+
+    // Hilt
+    alias(libs.plugins.hilt.android)
+
+    // KSP
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = Namespaces.featureBar
+}
+
+dependencies {
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
