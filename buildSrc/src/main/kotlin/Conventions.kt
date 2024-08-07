0ac7ca5c0c145e32def1928f77076111950cb5db
TheAlisher <allisher.abenov@gmail.com>
2024-08-07 13:25:57 +0600

    removed buildSrc module

--- buildSrc/src/main/kotlin/Conventions.kt
+++
@@ -1,7 +0,0 @@
-object Conventions {
-
-    const val kotlinLibrary = "kotlin-library-conventions"
-    const val androidApp = "android-app-conventions"
-    const val androidLibrary = "android-library-conventions"
-    const val androidFeature = "android-feature-conventions"
-}
