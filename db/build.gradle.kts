8f37d9bf4812a34ad20d908e497e50252252bc1a
TheAlisher <allisher.abenov@gmail.com>
2024-08-14 00:26:46 +0600

    Removed unnecessary gradle-convention and module dependencies

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
-    alias(libs.plugins.convention.android.feature)
+    alias(libs.plugins.convention.android.library)
 }
 
 dependencies {
 
+    // Data
+    implementation(projects.core.data)
+
     // Room
     ksp(libs.androidx.room.compiler)
 
-    // Modules
+    // Feature
     implementation(projects.feature.foo)
     implementation(projects.feature.bar)
 }
