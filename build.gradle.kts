f5f4d6bb6de68f892a747438702b59ea4ea9c927
TheAlisher <allisher.abenov@gmail.com>
2024-08-14 00:41:35 +0600

    removed unnecessary

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import dev.iurysouza.modulegraph.Theme
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -15,15 +13,4 @@ plugins {
 
     // KSP
     alias(libs.plugins.ksp) apply false
-
-    // Module Graph
-    id("dev.iurysouza.modulegraph") version "0.10.0"
-}
-
-moduleGraphConfig {
-    readmePath.set("./GRAPH.md")
-    heading = "### Module Graph"
-    setStyleByModuleType.set(true)
-    rootModulesRegex.set(".*app.*")
-    theme.set(Theme.DARK)
 }
