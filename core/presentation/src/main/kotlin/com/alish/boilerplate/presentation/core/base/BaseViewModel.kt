a72d911b539bc482dbb929f758c638944d840e14
TheAlisher <allisher.abenov@gmail.com>
2024-09-12 01:35:23 +0600

    removed unnecessary in BaseViewModel

--- core/presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/base/BaseViewModel.kt
+++ core/presentation/src/main/kotlin/com/alish/boilerplate/presentation/core/base/BaseViewModel.kt
@@ -2,9 +2,6 @@ package com.alish.boilerplate.presentation.core.base
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
-import androidx.paging.map
 import com.alish.boilerplate.domain.core.Either
 import com.alish.boilerplate.domain.core.NetworkError
 import com.alish.boilerplate.presentation.core.UIState
@@ -12,7 +9,6 @@ import com.alish.boilerplate.presentation.core.reset
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 /**
