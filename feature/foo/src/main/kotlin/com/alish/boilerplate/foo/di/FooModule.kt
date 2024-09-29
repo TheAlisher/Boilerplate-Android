e809c6219a032e1a94c1c451fd5e66c625b3b6af
TheAlisher <allisher.abenov@gmail.com>
2024-09-29 18:01:20 +0600

    removed unnecessary FooRepoModule

--- feature/foo/src/main/kotlin/com/alish/boilerplate/foo/di/FooModule.kt
+++ feature/foo/src/main/kotlin/com/alish/boilerplate/foo/di/FooModule.kt
@@ -2,9 +2,6 @@ package com.alish.boilerplate.foo.di
 
 import com.alish.boilerplate.data.remote.NetworkClient
 import com.alish.boilerplate.foo.data.network.apiservice.FooApiService
-import com.alish.boilerplate.foo.data.repository.FooRepositoryImpl
-import com.alish.boilerplate.foo.domain.repository.FooRepository
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,14 +17,4 @@ object FooModule {
     fun provideFooApiService(
         client: NetworkClient
     ): FooApiService = client.provideApiService()
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-abstract class FooRepoModule {
-
-    @Binds
-    abstract fun bindFooRepository(
-        repositoryImpl: FooRepositoryImpl
-    ): FooRepository
 }
