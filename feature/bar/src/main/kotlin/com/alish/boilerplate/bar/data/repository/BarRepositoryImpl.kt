ad0170d369c25b0f8deaaa5a6d2f61557e53177b
TheAlisher <allisher.abenov@gmail.com>
2024-09-29 18:01:52 +0600

    added @BoundTo annotation from Hilt Extension

--- feature/bar/src/main/kotlin/com/alish/boilerplate/bar/data/repository/BarRepositoryImpl.kt
+++ feature/bar/src/main/kotlin/com/alish/boilerplate/bar/data/repository/BarRepositoryImpl.kt
@@ -5,8 +5,10 @@ import com.alish.boilerplate.data.core.utils.onSuccess
 import com.alish.boilerplate.bar.data.db.dao.BarDao
 import com.alish.boilerplate.bar.data.network.apiservice.BarApiService
 import com.alish.boilerplate.bar.domain.repository.BarRepository
+import it.czerwinski.android.hilt.annotations.BoundTo
 import javax.inject.Inject
 
+@BoundTo(supertype = BarRepository::class)
 class BarRepositoryImpl @Inject constructor(
     private val service: BarApiService,
     private val dao: BarDao
