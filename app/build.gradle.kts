2cc5e31a0abf19d838136da3f55030de3ed2c42a
TheAlisher <allisher.abenov@gmail.com>
2026-06-10 02:46:52 +0500

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 
 dependencies {
 
+    // TODO: remove after Dagger Hilt update kotlin metadata version [if they update ;)]
+    ksp("org.jetbrains.kotlin:kotlin-metadata-jvm:2.4.0")
+
     // Database
     implementation(projects.db)
 
